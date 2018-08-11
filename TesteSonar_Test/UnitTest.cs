f6921e5fcfd87fe98adb9b24956992887d4e9e5f
leo_s <leo_s@DESKTOPLEOR1UV1>
2018-08-11 17:25:04 -0300

    Inclusão Projeto de Teste

---
+++ TesteSonar_Test/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TesteSonar_Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TesteMetodoUm()
+        {
+        }
+    }
+}
